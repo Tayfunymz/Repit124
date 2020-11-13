017979b948d17ad41e6da762232eff19bf607a62
Tayfun <tayfunymz@gmail.com>
2020-11-13 11:14:59 -0800

    Shared Replit Question 124 so we can collaborate on it together

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] nums = {input.nextInt(), input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+        //TODO: write your code below
+
+
+
+        //Do not change below statement:
+        System.out.println(Arrays.toString(nums));
+
+    }
+}
