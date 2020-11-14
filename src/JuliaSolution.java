0f3c7db25638377af6d774067f42e462a41a1856
Tayfunymz <74079478+Tayfunymz@users.noreply.github.com>
2020-11-14 13:24:11 -0800

    Update JuliaSolution.java

--- src/JuliaSolution.java
+++ src/JuliaSolution.java
@@ -5,10 +5,15 @@ public class JuliaSolution {
         int temp = nums[0];
 
    //you saying its easy one i cant do that one ,i dont understand ...
+   // To make it easy create a new int[] nums2 = new int[nums.length]
+   // Then iterate through the nums array starting from the end going to the beggining 
+   // and add each element to your new array.
+        
 
 
 
         // DO NOT TOUCH BELOW CODE PLEASE
+        // IF you make a new array CHANGE return nums to -> return yournewarray
         return nums;
     }
 }
