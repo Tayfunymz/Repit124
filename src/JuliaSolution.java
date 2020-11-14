31d75a6e10df86a8aae954f325bd57b069d683e2
JuliaCybertek <juliapudovikova@gmail.com>
2020-11-13 21:13:21 -0700

    no sulution

--- src/JuliaSolution.java
+++ src/JuliaSolution.java
@@ -2,7 +2,9 @@ public class JuliaSolution {
     public static int[] solution() {
         int[] nums = {4,3,2,44,1,100,55};
         //TODO: write your code below
+        int temp = nums[0];
 
+   //you saying its easy one i cant do that one ,i dont understand ...
 
 
 
