cc24e04c7c1f2d52519b0e5dd4d680e785b406cb
JuliaCybertek <juliapudovikova@gmail.com>
2020-11-15 22:02:36 -0700

    Same result

--- src/JuliaSolution.java
+++ src/JuliaSolution.java
@@ -1,15 +1,26 @@
+import java.util.Arrays;
+
 public class JuliaSolution {
     public static int[] solution() {
         int[] nums = {4,3,2,44,1,100,55};
         //TODO: write your code below
-        int temp = nums[0];
+        int [ ]reverse=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--) {
+            for(int j=0;j>= nums.length-1;j++){
+                reverse[j]+=nums[i];
+            }
+
+            }
+// STILL PRINTS ME IN SAME ORDER :(
+
+
 
    //you saying its easy one i cant do that one ,i dont understand ...
    // To make it easy create a new int[] nums2 = new int[nums.length]
    // Then iterate through the nums array starting from the end going to the beggining 
    // and add each element to your new array.
-        
 
+        System.out.println(Arrays.toString(nums));
 
 
         // DO NOT TOUCH BELOW CODE PLEASE
